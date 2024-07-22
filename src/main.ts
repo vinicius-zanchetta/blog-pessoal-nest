7e0a4ed565ea8ff31dd5d40db5b16bd15917c54a
vinicius zanchetta <viniciuszanchettadev@gmail.com>
2024-07-22 09:16:28 -0300

    Commit inicial, adicionados pacotes, alterada porta para 4000 e definido TIMEZONE para -03:00

---
+++ src/main.ts
@@ -0,0 +1,11 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+  
+  process.env.TZ = '-03:00';
+
+  await app.listen(4000);
+}
+bootstrap();
